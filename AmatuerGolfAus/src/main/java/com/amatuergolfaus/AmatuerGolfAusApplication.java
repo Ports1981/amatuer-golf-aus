720cb7b3ab5ba3cc7efd222d8b59a815b6aa26dc
mat <mat@mat-Dell-DXP061>
2018-08-22 10:25:57 +1000

    Initial check-in of the Amatuer Golf Aus

---
+++ AmatuerGolfAus/src/main/java/com/amatuergolfaus/AmatuerGolfAusApplication.java
@@ -0,0 +1,12 @@
+package com.amatuergolfaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmatuerGolfAusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmatuerGolfAusApplication.class, args);
+	}
+}
